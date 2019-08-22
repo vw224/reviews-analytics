0f5c2eae02de6001e26c491b548b0cc7741d76ca
Vivi <vvw224@gmail.com>
2019-08-22 13:24:15 +0800

    version 1.2

--- read.py
+++ read.py
@@ -1,4 +1,3 @@
-
 data = []
 count = 0
 with open('reviews.txt', 'r') as f:
@@ -9,13 +8,17 @@ with open('reviews.txt', 'r') as f:
 			print(len(data))
 
 length = 0
+x = 0
 for i in range(len(data)):
 	length += len(data[i])
+	if len(data[i]) < 100:
+		x += 1
 
 avg = length / count
 
 print('檔案讀取完了，總共有', len(data), '筆資料')
 print('留言平均長度為', avg)
+print('留言長度小於100，共有', x, "筆")
 
 
 """
@@ -36,4 +39,10 @@ for d in data:
 	sum_len += len(d)
 
 print('留言的平均為', sum_len/len(data))
+
+new = []
+for d in data:
+	if len(d) < 100:
+		new.append(d)
+print('一共有', len(new), '筆留言長度小於100')
 """
