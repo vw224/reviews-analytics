9a0f3cc0c1b04df61119aa6b02b668ab6803f6df
Vivi <vvw224@gmail.com>
2019-08-22 14:07:43 +0800

    vision 2.0

--- read.py
+++ read.py
@@ -1,24 +1,33 @@
-data = []
+#1. 每100000筆，印出狀態
+#2. 印出 共有幾筆資料?
+#3. 印出 留言平均長度?
+#4. 印出 留言長度<100，共有幾筆?
+#5. 印出 隨機一筆留言長度<100的留言
+
+data =[]
 count = 0
 with open('reviews.txt', 'r') as f:
-	for line in f:
-		data.append(line)
+	for all_review in f:
+		data.append(all_review)
 		count += 1
-		if count % 100000 == 0:
-			print(len(data))
+		if count%100000 == 0:
+			print(count)
+print('共有', count,'筆資料')
 
-length = 0
-x = 0
-for i in range(len(data)):
-	length += len(data[i])
-	if len(data[i]) < 100:
-		x += 1
+sum_review = 0
 
-avg = length / count
+for d in data:
+	sum_review += len(d)
+#print('留言長度共', sum_review)
+print('留言平均長度為', sum_review/len(data))
 
-print('檔案讀取完了，總共有', len(data), '筆資料')
-print('留言平均長度為', avg)
-print('留言長度小於100，共有', x, "筆")
+under100 = []
+for x in data:
+	if len(x) < 100:
+		under100.append(x)
+print('留言長度小於100的，共有', len(under100), '筆')
+print(under100[0])
+print(under100[1])
 
 
 """
